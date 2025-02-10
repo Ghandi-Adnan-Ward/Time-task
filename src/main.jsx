d8cb376919aedfadb546258645ca80c865c7617f
ghandi ward <ghandi.ward81@gmail.com>
2025-02-10 12:49:42 +0300

    fix problem 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, {   } from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router,   } from "react-router-dom";
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router basename='/Time-task'>
-    <App />
 
-    </Router>
-  </StrictMode>,
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  
+  <React.StrictMode>
+
+<Router basename='/Time-task'>
+      <App/>
+      </Router>
+
+   </React.StrictMode>,
 )
