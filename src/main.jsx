0435e6cb6793e70a53602801e5e1b97d9e95d1d4
ghandi ward <ghandi.ward81@gmail.com>
2025-02-12 21:40:36 +0300

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,14 @@ import './index.css'
 import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router,   } from "react-router-dom";
+import { HashRouter   } from "react-router-dom";
 
 const root=ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   
   <React.StrictMode>
-
-<Router basename='/Time-task'>
-      <App/>
-      </Router>
-
+      <HashRouter basename='/'>
+        <App/>
+      </HashRouter>
    </React.StrictMode>,
 )
